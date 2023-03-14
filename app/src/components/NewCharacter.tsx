76f879b873bd3aadcc1ebbf4da9531d44d1f410e
Daniel <danielukoha101@gmail.com>
2023-03-14 15:18:45 +0100

    add character input field and submit button

--- app/src/components/NewCharacter.tsx
+++ app/src/components/NewCharacter.tsx
@@ -1,5 +1,21 @@
+import { Flex, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+
 const NewCharacter = () => {
-  return <section></section>;
+  return (
+    <section>
+      <Flex maxW="35rem" mx="auto" mt="4" py="8" px="4">
+        <form style={{ width: "100%" }}>
+          <FormControl>
+            <FormLabel>Characters</FormLabel>
+            <Input placeholder="Enter Character" />
+          </FormControl>
+          <Button mt="4" colorScheme="teal" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Flex>
+    </section>
+  );
 };
 
 export default NewCharacter;
