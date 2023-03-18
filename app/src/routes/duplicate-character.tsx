88459f3f890284d5f734a292e61670b0d3358036
Daniel <danielukoha101@gmail.com>
2023-03-18 14:04:15 +0100

    make route accessible for valid input submission

--- app/src/routes/duplicate-character.tsx
+++ app/src/routes/duplicate-character.tsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import DuplicateCharacter from "../components/DuplicateCharacter";
+import { CharacterContext } from "../context/character-context";
 
 const DuplicateCharacterPage = () => {
+  const { characters } = useContext(CharacterContext);
+
+  if (!characters) {
+    return <Navigate to="/" />;
+  }
+
   return <DuplicateCharacter />;
 };
 
