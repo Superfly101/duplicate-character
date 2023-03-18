186968c3114cfa53f4e6cc25386ee3ae88f11921
Daniel <danielukoha101@gmail.com>
2023-03-18 13:27:32 +0100

    generate card color from character

--- app/src/components/CharacterItem.tsx
+++ app/src/components/CharacterItem.tsx
@@ -7,11 +7,29 @@ type CardProps = {
 };
 
 const CharacterItem = ({ children, onRemove }: CardProps) => {
+  const getCardColor = (char: string) => {
+    // set character in upper case
+    const alphabet = char.toLowerCase();
+
+    // get the ASCII code of the character
+    const asciiCode = alphabet.charCodeAt(0);
+
+    // number that contains 3 times ASCII value of character -- unique for every alphabet
+    const colorNum =
+      asciiCode.toString() + asciiCode.toString() + asciiCode.toString();
+
+    let num = Math.round(0xffffff * parseInt(colorNum));
+    let r = (num >> 16) & 255;
+    let g = (num >> 8) & 255;
+    let b = num & 255;
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  };
   return (
     <Flex
       direction="column-reverse"
       align="center"
-      bg="yellow"
+      bg={getCardColor(children!.toString())}
       p="4"
       gap="2"
       borderRadius="lg"
